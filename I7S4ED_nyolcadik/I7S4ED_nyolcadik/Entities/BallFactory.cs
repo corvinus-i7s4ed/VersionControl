655b08a89669b2007bb131baade8645e0840338f
Fekete Áron <aron.fekete@stud.uni-corvinus.hu>
2021-10-31 16:08:10 +0100

    Labdagyár létrehozása

---
+++ I7S4ED_nyolcadik/I7S4ED_nyolcadik/Entities/BallFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I7S4ED_nyolcadik.Entities
+{
+    class BallFactory
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
