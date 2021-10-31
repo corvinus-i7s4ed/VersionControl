e8ed0244487a3a9191d14275928220d45c46d3ce
Fekete Áron <aron.fekete@stud.uni-corvinus.hu>
2021-10-31 17:06:44 +0100

    Termékválasztó gombok

--- I7S4ED_nyolcadik/I7S4ED_nyolcadik/Form1.cs
+++ I7S4ED_nyolcadik/I7S4ED_nyolcadik/Form1.cs
@@ -16,15 +16,18 @@ namespace I7S4ED_nyolcadik
     {
         List<Toy> _toys=new List<Toy>();
 
-        private IToyFactory _factory;
+        private Toy _nextToy;
 
+        private IToyFactory _factory;
         public IToyFactory Factory
         {
             get { return _factory; }
-            set { _factory = value; }
+            set { _factory = value;
+                DisplayNext();
+            }
         }
 
-
+        
 
         public Form1()
         {
@@ -57,6 +60,26 @@ namespace I7S4ED_nyolcadik
                 mainPanel.Controls.Remove(oldestToy);
                 _toys.Remove(oldestToy);
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory();
+        }
+
+        private void DisplayNext()
+        {
+            if (_nextToy != null)
+                Controls.Remove(_nextToy);
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = label1.Top + label1.Height + 20;
+            _nextToy.Left = label1.Left;
+            Controls.Add(_nextToy);
         }
     }
 }
