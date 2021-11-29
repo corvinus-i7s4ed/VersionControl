528689350da296bb442a9a449c755fc6286ad3d4
Fekete Áron <aron.fekete@stud.uni-corvinus.hu>
2021-11-29 15:49:05 +0100

    Osztályok létrehozása

---
+++ I7S4ED_kilencedik/I7S4ED_kilencedik/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I7S4ED_kilencedik.Entities
+{
+    public class BirthProbability
+    {
+        public int Age { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double Probability { get; set; }
+    }
+}
---
+++ I7S4ED_kilencedik/I7S4ED_kilencedik/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I7S4ED_kilencedik.Entities
+{
+    public class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+        public double Probability { get; set; }
+    }
+}
