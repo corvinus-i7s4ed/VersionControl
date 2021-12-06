343b5229f05f5724841cf01b9102a50148ccdffe
Fekete Áron <aron.fekete@stud.uni-corvinus.hu>
2021-12-06 12:21:43 +0100

    Induló populáció felépítése

--- I7S4ED_tizedik/I7S4ED_tizedik/Form1.cs
+++ I7S4ED_tizedik/I7S4ED_tizedik/Form1.cs
@@ -15,6 +15,10 @@ namespace I7S4ED_tizedik
     {
         GameController gc = new GameController();
         GameArea ga;
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
 
         public Form1()
         {
@@ -22,8 +26,13 @@ namespace I7S4ED_tizedik
 
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
-            gc.AddPlayer();
-            gc.Start(true);
+           
+            
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+            gc.Start();
         }
     }
 }
